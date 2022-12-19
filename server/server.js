1f85b692e0390475ce7217f4763c3c9cd88667ac
hieu <xst-le009660@stthomas.edu>
2022-12-18 20:02:12 -0600

    revert prev changes

---
+++ server/server.js
@@ -0,0 +1,337 @@
+// Built-in Node.js modules
+let fs = require('fs');
+let path = require('path');
+var cors = require('cors')
+
+// NPM modules
+let express = require('express');
+let sqlite3 = require('sqlite3');
+
+
+let db_filename = path.join(__dirname, 'db', 'stpaul_crime.sqlite3');
+
+let app = express();
+let port = 8000;
+
+app.use(express.json());
+app.use(cors());
+
+// Open SQLite3 database (in read-only mode)
+let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.log('Error opening ' + path.basename(db_filename));
+    }
+    else {
+        console.log('Now connected to ' + path.basename(db_filename));
+    }
+});
+
+
+// GET request handler for crime codes
+app.get('/codes', (req, res) => {
+    // console.log(req.query); // query object (key-value pairs after the ? in the url)
+    // console.log(Object.entries(req.query).length); // query object (key-value pairs after the ? in the url)
+    
+    let query = 'SELECT * FROM Codes ORDER BY code ASC';
+    if(Object.entries(req.query).length == 0) {
+        databaseSelect(query)
+            .then((rows) => {
+                // console.log(rows);
+                res.status(200).type('json').send(rows); 
+            })
+            .catch((err) => {
+                // console.log('ERROR: ' + err);
+                res.status(500).type('text').send('Error: ' + err); 
+            });
+    } else {
+        let codes = req.query.code.split(',');
+        // console.log(codes);
+        query = 'SELECT * FROM Codes WHERE code = ?';
+
+        for(let i = 1; i < codes.length; i++) {
+            query += ' OR code = ?';
+        }
+
+        query += ' ORDER BY code ASC';
+
+        // console.log('LONG QUERY: ' + query);
+
+        databaseSelect(query,codes)
+            .then((rows) => {
+                // console.log(rows);
+                res.status(200).type('json').send(rows); 
+            })
+            .catch((err) => {
+                // console.log('ERROR: ' + err);
+                res.status(500).type('text').send('Error: ' + err); 
+            });
+    }
+});
+
+// GET request handler for neighborhoods
+app.get('/neighborhoods', (req, res) => {
+    // console.log(req.query); // query object (key-value pairs after the ? in the url)
+
+    let query = 'SELECT * FROM Neighborhoods ORDER BY neighborhood_number ASC';
+    if(Object.entries(req.query).length == 0) {
+        databaseSelect(query)
+            .then((rows) => {
+                res.status(200).type('json').send(rows);
+            })
+            .catch((err) => {
+                // console.log('ERROR: ' + err);
+                res.status(500).type('text').send('Error: ' + err); 
+            })
+    } else {
+        let neighborhoods = req.query.id.split(',');
+        query = 'SELECT * FROM Neighborhoods WHERE neighborhood_number = ?';
+
+        for(let i = 1; i < neighborhoods.length; i++) {
+            query += ' OR neighborhood_number = ?';
+        }
+
+        query += ' ORDER BY neighborhood_number ASC';
+
+        // console.log('LONG QUERY: ' + query);
+
+        databaseSelect(query,neighborhoods)
+            .then((rows) => {
+                // console.log(rows);
+                res.status(200).type('json').send(rows); 
+            })
+            .catch((err) => {
+                // console.log('ERROR: ' + err);
+                res.status(500).type('text').send('Error: ' + err); 
+            });
+    }
+    
+    // res.status(200).type('json').send({}); // <-- you will need to change this
+});
+
+// GET request handler for crime incidents
+app.get('/incidents', (req, res) => {
+    // console.log(req.query); // query object (key-value pairs after the ? in the url)
+    
+    let query = 'SELECT * FROM Incidents ORDER BY date_time DESC LIMIT 1000';
+    if(Object.entries(req.query).length == 0) {
+        databaseSelect(query)
+            .then((rows) => {
+                //separate date and time
+                for(let i = 0; i < rows.length; i++) {
+                    let split = rows[i].date_time.split('T');
+                    rows[i].date = split[0];
+                    rows[i].time = split[1];
+                    delete rows[i].date_time;
+                }
+                res.status(200).type('json').send(rows);
+            })
+            .catch((err) => {
+                // console.log('ERROR: ' + err);
+                res.status(500).type('text').send('Error: ' + err); 
+            })
+    } else {
+        let first = true;
+        let params =[];
+        // console.log(req.query);
+        
+        let query = 'SELECT * FROM Incidents';
+        if(req.query.hasOwnProperty('start_date')) {
+            params.push(req.query.start_date);
+            if(first) {
+                query += ' WHERE date(date_time) >= ?'
+                first = false;
+            } else {
+                query += ' AND date(date_time) >= ?'
+            }
+        }
+        if(req.query.hasOwnProperty('end_date')) {
+            params.push(req.query.end_date);
+            if(first) {
+                query += ' WHERE date(date_time) <= ?'
+                first = false;
+            } else {
+                query += ' AND date(date_time) <= ?'
+            }
+        }
+        if(req.query.hasOwnProperty('code')) {
+            let firstCode = true;
+            let codes = req.query.code.split(',');
+            // console.log('CODES:');
+            // console.log(codes);
+            for(let i = 0 ; i < codes.length; i++) {
+                params.push(codes[i]);
+                if(firstCode) {
+                    if(first) {
+                        query += ' WHERE code IN (?';
+                        first = false;
+                        firstCode = false;
+                    } else {
+                        query += ' AND code IN (?';
+                        firstCode = false;
+                    }
+                } else {
+                    query += ', ?';
+                }
+            }
+            query += ')';
+        }
+        if(req.query.hasOwnProperty('grid')) {
+            let firstGrid = true;
+            let grids = req.query.grid.split(',');
+            // console.log('GRIDS:');
+            // console.log(grids);
+            for(let i = 0 ; i < grids.length; i++) {
+                params.push(grids[i]);
+                if(firstGrid) {
+                    if(first) {
+                        query += ' WHERE police_grid IN (?';
+                        first = false;
+                        firstGrid = false;
+                    } else {
+                        query += ' AND police_grid IN (?';
+                        firstGrid = false;
+                    }
+                } else {
+                    query += ', ?';
+                }
+            }
+            query += ')';
+        }
+        if(req.query.hasOwnProperty('neighborhood')) {
+            let firstNeighborhood = true;
+            let neighborhoods = req.query.neighborhood.split(',');
+            // console.log('Neighborhoods:');
+            // console.log(neighborhoods);
+            for(let i = 0 ; i < neighborhoods.length; i++) {
+                params.push(neighborhoods[i]);
+                if(firstNeighborhood) {
+                    if(first) {
+                        query += ' WHERE neighborhood_number IN (?';
+                        first = false;
+                        firstNeighborhood = false;
+                    } else {
+                        query += ' AND neighborhood_number IN (?';
+                        firstNeighborhood = false;
+                    }
+                } else {
+                    query += ', ?';
+                }
+            }
+            query += ')';
+        }
+        // console.log('  ' + params);
+        query += ' ORDER BY date_time DESC';
+        if(req.query.hasOwnProperty('limit')) {
+            params.push(req.query.limit);
+            query += ' LIMIT ?';
+        } else {
+            query += ' LIMIT 1000';
+        }
+        // console.log(query);
+        databaseSelect(query, params)
+            .then((rows) => {
+                // console.log(rows);
+                // console.log(query);
+                for(let i = 0; i < rows.length; i++) {
+                    let split = rows[i].date_time.split('T');
+                    rows[i].date = split[0];
+                    rows[i].time = split[1];
+                    delete rows[i].date_time;
+                }
+                res.status(200).type('json').send(rows); 
+            })
+            .catch((err) => {
+                // console.log(err);
+                res.status(400).type('text').send('ERROR: ' + err); 
+            })
+    }
+
+    // res.status(200).type('json').send({}); // <-- you will need to change this
+});
+
+// PUT request handler for new crime incident
+app.put('/new-incident', (req, res) => {
+    databaseSelect('SELECT * FROM Incidents WHERE case_number = ?', req.body.case_number)
+        .then((rows) => {
+            if (rows.length > 0) {
+                res.status(500).type('text').send('The case number already exists');
+            } else {
+                var {
+                    case_number,
+                    date,
+                    time,
+                    code,
+                    incident,
+                    police_grid,
+                    neighborhood_number,
+                    block
+                } = req.body;
+                var insertQuery = 'INSERT INTO Incidents (case_number, date_time, code, incident, police_grid, neighborhood_number, block) VALUES (?, ?, ?, ?, ?, ?, ?);'
+                databaseRun(insertQuery, [case_number, date + 'T' + time, code, incident, police_grid, neighborhood_number, block])
+                    .then(() => {
+                        res.status(200).type('text').send('Uploaded successfully');
+                    })
+                    .catch((err) => {
+                        res.status(400).type('text').send('ERROR: ' + err);
+                    })
+            }
+        })
+        .catch((err) => {
+            res.status(400).type('text').send('ERROR: ' + err);
+        })
+});
+
+// DELETE request handler for new crime incident
+app.delete('/remove-incident', (req, res) => {
+    databaseSelect('SELECT * FROM Incidents WHERE case_number = ?', req.body.case_number)
+        .then((rows) => {
+            if (rows.length === 0) {
+                res.status(500).type('text').send('The case number does not exist');
+            } else {
+                databaseRun('DELETE FROM Incidents WHERE case_number = ?', req.body.case_number)
+                    .then(() => {
+                        res.status(200).type('text').send('Deleted successfully');
+                    })
+                    .catch((err) => {
+                        res.status(400).type('text').send('ERROR: ' + err);
+                    })
+            }
+        })
+        .catch((err) => {
+            res.status(400).type('text').send('ERROR: ' + err);
+        })
+});
+
+// Create Promise for SQLite3 database SELECT query 
+function databaseSelect(query, params) {
+    return new Promise((resolve, reject) => {
+        db.all(query, params, (err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(rows);
+            }
+        })
+    })
+}
+
+// Create Promise for SQLite3 database INSERT or DELETE query
+function databaseRun(query, params) {
+    return new Promise((resolve, reject) => {
+        db.run(query, params, (err) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        });
+    })
+}
+
+
+// Start server - listen for client connections
+app.listen(port, () => {
+    console.log('Now listening on port ' + port);
+});
---
+++ ui/src/components/DataFilter/DataFilter.vue
@@ -0,0 +1,238 @@
+<template>
+    <div class="filter">
+    <div class="cell auto">
+      <button class="button" type="button" data-toggle="data-filter">Filter</button>
+      <img class="legend" src="./assets/legend.png" alt="table legend" />
+    </div>
+      <div class="dropdown-pane form cell-auto" id="data-filter" data-dropdown data-auto-focus="true">
+        <div class="grid-x" >
+            <div class="small-10">
+                <h4>Filter results</h4>
+            </div>
+            <div class="small-2">
+                <button class="close-button" aria-label="Dismiss alert" type="button" data-toggle="data-filter">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+        <form>
+            <div class="grid-container">
+                <div class="grid-x grid-padding-x">
+                    <div class="cell small-6">
+                        <h6>Neighborhood:</h6>
+                        <template v-for="(value, key) in neighborhoods">
+                            <input type="checkbox" :id=value :value=key v-model="settings.neighborhoods">
+                            <label :for=value>{{value}}</label>
+                            <br/>
+                        </template>
+                    </div>
+                    <div class="cell small-6">
+                        <h6>Incident Type:</h6>
+                        <template v-for="(value, key) in type_groups">
+                            <input type="checkbox" :id=key :value=key v-model="settings.types">
+                            <label :for=key>{{key}}</label>
+                            <br/>
+                        </template>
+                    </div>
+                </div>
+            </div>
+            <h6>Timespan:</h6>
+                <div class="grid-x">
+                    <div class="small-3 margin-r">
+                        Start: <input type="date" @change="showStartTime" v-model="settings.start_date">
+                    </div>
+                    <div class="small-3 margin-r">
+                        End: <input type="date" @change="showEndTime" v-model="settings.end_date">
+                    </div>
+                </div>
+                <div class="grid-x">
+                    <div v-if="show_start_time" class="small-3 margin-r">
+                        <input type="time" v-model="settings.start_time">
+                    </div>
+                    <div v-if="show_end_time" class="small-3 margin-r">
+                        <input type="time" v-model="settings.end_time">
+                    </div>
+                </div>
+             <h6>Max Incidents:</h6>
+             <div class="grid-x">
+                <div class="small-2">
+                    <input required type="number" min="1" v-model="settings.limit">  
+                </div>
+            </div>
+            <button type="button" class="button margin-r" @click="applyFilters">Apply Filters</button>
+            <button type="button" class="button" @click="resetFilters">Reset Filters</button>
+        </form>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        this.data_filter = new Foundation.Dropdown($('#data-filter'), {
+            vOffset: 5
+        });
+    },
+    data() {
+        return {
+            settings: {
+                types: [],
+                neighborhoods: [],
+                start_date: "",
+                end_date: "",
+                start_time: "00:00:00",
+                end_time: "23:59:59",
+                limit: ""
+            },
+            neighborhoods: {1:'Conway/Battlecreek/Highwood', 2:'Greater East Side', 3:'West Side', 4:'Daytons Bluff', 5:'Payne/Phalen',6:'North End',7:'Thomas/Dale(Frogtown)', 8:'Summit/University', 9:'West Seventh', 10:'Como', 11:'Hamline/Midway', 12:'St. Anthony', 13:'Union Park', 14:'Macalester-Groveland', 15:'Highland', 16:'Summit Hill', 17:'Capitol River'},
+            type_groups: {
+                "Murder": [100, 110, 120],
+                "Rape" : [210, 220],
+                "Robbery": [300, 311, 312, 313, 314, 321, 322, 323, 324, 331, 333, 334, 341, 342, 343, 344, 351, 352, 353, 354, 361, 363, 364, 371, 372, 373, 374],
+                "Assault": [ 400, 410, 411, 412, 420, 421, 422, 430, 431, 432, 440, 441, 442, 450, 451, 452, 453, 810, 861, 862, 863],
+                "Burglary": [500, 510, 511, 513, 515, 516, 520, 521, 523, 525, 526, 530, 531, 533, 535, 536, 540, 541, 543, 545, 546, 550, 551, 553, 555, 556, 560, 561, 563, 565, 566],
+                "Theft": [600, 603, 611, 612, 613, 614, 621, 622, 623, 630, 631, 632, 633, 640, 641, 642, 643, 651, 652, 653, 661, 662, 663, 671, 672, 673, 681, 682, 683, 691, 692, 693],
+                "Motor Vehicle Theft": [700, 710, 711, 712, 720, 721, 722, 730, 731, 732],
+                "Arson": [900, 901, 903, 905, 911, 913, 915, 921, 922, 923, 925, 931, 933, 941, 942, 951, 961, 971, 972, 975, 981, 982],
+                "Vandalism": [1400, 1401, 1410, 1415, 1416, 1420, 1425, 1426, 1430, 1435, 1436],
+                "Narcotics": [1800, 1810, 1811, 1812, 1813, 1814, 1815, 1820, 1822, 1823, 1824, 1825, 1830, 1835, 1840, 1841, 1842, 1843, 1844, 1845, 1850, 1855, 1860, 1865, 1870, 1880, 1885],
+                "Other": [2619, 3100, 9954, 9959, 9986]
+            },
+            show_start_time: false,
+            show_end_time: false
+        }
+    },
+    methods: {
+        showStartTime() {
+            this.show_start_time = true;
+        },
+        showEndTime() {
+            this.show_end_time = true;
+        },
+        applyFilters() {
+            if(this.settings.start_time.length < 6) this.settings.start_time += ":00";
+            let time = true;
+            if(this.settings.start_time == "00:00:00" && this.settings.end_time == "23:59:59")time = false;
+            let url = this.generateURL();
+            let test;
+            if(time) {
+                this.$parent.getJSON(url)
+                .then((results) => {
+                    test = this.filterTime(results, this.settings.start_time, this.settings.end_time);
+                    console.log(test);
+                    this.$parent.updateIncidents(test);
+                })
+                .catch((err) => {
+                    console.log("Error retrieving data" + err);
+                });
+            } else {
+                console.log("no time filter");
+                this.$parent.getJSON(url)
+                .then((results) => {
+                    test = results;
+                    console.log(test);
+                    this.$parent.updateIncidents(test);
+                })
+                .catch((err) => {
+                    console.log("Error retrieving data" + err);
+                });
+            }
+            
+        },
+        generateURL() {
+            let url = "http://localhost:8000/incidents?";
+            let firstParam = true;
+            for(let i = 0; i < this.settings.types.length; i++) {
+                if(i == 0) {
+                    url += "code=";
+                    firstParam = false;
+                }
+                for(let j = 0; j < this.type_groups[this.settings.types[i]].length; j++) {
+                    if(i != 0 || j != 0) url += ",";
+                    url += this.type_groups[this.settings.types[i]][j];
+                }
+            }
+            for(let i = 0; i < this.settings.neighborhoods.length; i++) {
+                if(i == 0) {
+                    if(firstParam) {
+                        url += "neighborhood=" + this.settings.neighborhoods[i];
+                        firstParam = false;
+                    } else {
+                        url += "&neighborhood=" + this.settings.neighborhoods[i];
+                    }
+                } else {
+                    url += "," + this.settings.neighborhoods[i];
+                }
+            }
+            if(this.settings.start_date != undefined && this.settings.start_date != "") {
+                let start = "";
+                if(!firstParam) {
+                    start = "&";
+                }
+                start += "start_date=" + this.settings.start_date + "T" + this.settings.start_time;
+                url += start;
+                firstParam = false;
+            }
+            if(this.settings.end_date != undefined && this.settings.end_date != "") {
+                let end = "";
+                if(!firstParam) {
+                    end = "&";
+                }
+                end += "end_date=" + this.settings.end_date + "T" + this.settings.end_time;
+                url += end;
+                firstParam = false;
+            }
+            if(this.settings.limit != undefined && this.settings.limit != "" ){
+                if(!firstParam) {
+                    url += "&";
+                }
+                url += "limit="  + this.settings.limit;
+            }
+            // console.log('URL:' + url);
+            //if no filters
+            if(url.charAt(url.length-1) == '?') url = url.substring(0,url.length-1);
+            return url;
+        },
+        filterTime(data, start, end) {
+            let filtered = [];
+            let idx = 0;
+            for(let i = 0; i < data.length; i++) {
+                if(data[i].time >= start && data[i].time <= end) {
+                    filtered[idx++] = data[i];
+                }
+            }
+            return filtered;
+        },
+        resetFilters() {
+            this.settings.types = [],
+            this.settings.neighborhoods = [],
+            this.settings.start_date = "",
+            this.settings.end_date = "",
+            this.settings.start_time = "00:00:00",
+            this.settings.end_time = "23:59:59",
+            this.settings.limit = ""
+        }
+    }
+}
+</script>
+
+<style >
+    .filter {
+        margin-top: .5rem;
+    }
+    .form {
+        width: 80%;
+        margin: 1rem;
+    }
+    .margin-r {
+        margin-right: 2.5%;
+    }
+    h6 {
+        font-weight: bold;
+    }
+    .legend {
+        position: relative;
+        clear: right;
+        left: 2 rem;
+    }
+</style>
